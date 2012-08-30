66aa2721c8ed597ad6d03f02b8a6fabf1dd1412d
Jan Kuča <jan@jankuca.com>
2012-08-30 02:19:51 +0200

    add pre-receive hook

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 
-cd /tmp
+CIAN_DIR=$(pwd)
 
 PUBLIC_KEY_NAME=$1
 LATEST_NODE_VERSION=$(./vendor/nave/nave.sh stable)
@@ -16,21 +16,27 @@ which foreman || gem install foreman
 
 which phantomjs || {
   apt-get install build-essential chrpath git-core libssl-dev libfontconfig1-dev
+  cd /tmp
   git clone git://github.com/ariya/phantomjs.git
   cd phantomjs
   git checkout 1.6
   ./build.sh
-  cd ..
+  cd $CIAN_DIR
 }
 
 
 # 1. gitolite
 
 useradd git
-mkdir "/home/git"
+mkdir /home/git
 cp "$PUBLIC_KEY_NAME" "/home/git/$PUBLIC_KEY_NAME"
-chown -R git:git "/home/git"
+chown -R git:git /home/git
 
-sudo -u git git clone "git://github.com/sitaramc/gitolite" "/home/git/gitolite"
-sudo -u git "/home/git/gitolite/install" -ln
-sudo -u git "/home/git/.bin/gitolite" setup -pk "/home/git/$PUBLIC_KEY_NAME"
+sudo -u git git clone git://github.com/sitaramc/gitolite /home/git/gitolite
+sudo -u git /home/git/gitolite/install -ln
+sudo -u git /home/git/.bin/gitolite setup -pk "/home/git/$PUBLIC_KEY_NAME"
+
+
+# 2. pre-receive git hook
+
+cp "$CIAN_DIR/pre-receive" /home/git/.gitolite/hooks/common/pre-receive
